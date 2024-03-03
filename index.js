677d1afb78318876343dcc100d43f47c5777b345
dyussenova <ddm_7891@mail.ru>
2024-03-03 12:49:15 +0400

    two commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mainEl = document.querySelector(".main");
 
 const inputEl = document.createElement("input");
 inputEl.classList.add("search-input");
+inputEl.type = "text";
 
 const dropdown = document.createElement("div");
 dropdown.classList.add("dropdown");
@@ -13,9 +14,60 @@ const cardCont = document.createElement("div");
 cardCont.classList.add("card-cont");
 
 dropdown.appendChild(dropdownList);
-mainEl.appendChild(inputEl);
-mainEl.appendChild(cardCont);
-mainEl.appendChild(dropdown);
+
+const fragment = document.createDocumentFragment();
+fragment.appendChild(inputEl);
+fragment.appendChild(dropdown);
+fragment.appendChild(cardCont);
+mainEl.appendChild(fragment);
+
+inputEl.addEventListener(
+  "input",
+  debounce(async (e) => {
+    const valueEvent = e.target.value.trim();
+    if (valueEvent) {
+      const response = await fetch(
+        `https://api.github.com/search/repositories?q=${valueEvent}&per_page=5`
+      );
+      if (response.ok) {
+        const data = await response.json();
+
+        data.items.forEach((repo) => createRepos(repo));
+      } else {
+        alert("Репозиторий не найден");
+      }
+    } else {
+      clearList();
+    }
+  }, 600)
+);
+
+function createRepos(repoData) {
+  const dropEl = document.createElement("li");
+  dropEl.classList.add("drop-elem");
+  dropEl.innerText = `${repoData.name}`;
+  dropEl.addEventListener("click", () => {
+    clearList();
+    let name = repoData.name;
+    let owner = repoData.owner.login;
+    let stars = repoData.stargazers_count;
+    const newRepos = document.createElement("div");
+    newRepos.classList.add("repos-card");
+    newRepos.innerHTML = `Name: ${name}<br>Owner: ${owner}<br>Stars: ${stars}`;
+
+    const btn = document.createElement("button");
+
+    btn.classList.add("btn-dlt");
+    btn.addEventListener("click", () => {
+      clearCard();
+    });
+    newRepos.appendChild(btn);
+
+    cardCont.appendChild(newRepos);
+    clearInput();
+  });
+  dropdownList.appendChild(dropEl);
+}
 
 function debounce(func, wait, immediate) {
   let timeout;
@@ -33,66 +85,14 @@ function debounce(func, wait, immediate) {
   };
 }
 
-function clearDrop() {
-  dropdown.innerHTML = " ";
+function clearInput() {
+  inputEl.value = " ";
 }
 
 function clearCard() {
   cardCont.innerHTML = " ";
 }
 
-function clearInput() {
-  inputEl.value = " ";
-}
-inputEl.addEventListener(
-  "keyup",
-  debounce(async (e) => {
-    try {
-      const inputvalue = inputEl.value.trim();
-      if (inputvalue) {
-        const response = await fetch(
-          `https://api.github.com/search/repositories?q=${inputvalue}&per_page=5`
-        );
-        if (response.ok) {
-          const data = await response.json();
-          data.items.forEach((repo) => createRepos(repo));
-        } else {
-          alert("Репозиторий не найден");
-        }
-      } else {
-        clearDrop();
-      }
-    } catch (err) {
-      console.log("Error:" + err);
-    }
-  }, 600)
-);
-
-function createRepos(repoData) {
-  const dropEl = document.createElement("li");
-  dropEl.classList.add("drop-elem");
-  dropEl.innerHTML = `<span>${repoData.name}</span>`;
-  dropEl.addEventListener("click", () => {
-    clearDrop();
-    let name = repoData.name;
-    let owner = repoData.owner.login;
-    let stars = repoData.stargazers_count;
-    const newRepos = document.createElement("div");
-    newRepos.classList.add("repos-card");
-    newRepos.innerHTML = `Name: ${name}<br>Owner: ${owner}<br>Stars: ${stars}`;
-
-    const btn = document.createElement("button");
-
-    btn.classList.add("btn-dlt");
-    btn.addEventListener("click", () => {
-      clearCard();
-    });
-
-    newRepos.appendChild(btn);
-
-    cardCont.appendChild(newRepos);
-    clearInput();
-  });
-
-  dropdownList.appendChild(dropEl);
+function clearList() {
+  dropdownList.innerHTML = " ";
 }
